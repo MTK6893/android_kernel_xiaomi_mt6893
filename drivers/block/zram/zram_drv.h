6821ed000e44edd168d2e216e0c0df96c4d341a4
Joonsoo Kim <iamjoonsoo.kim@lge.com>
2017-05-12 11:30:03 +0900

    zram: make deduplication feature optional
    Benefit of deduplication is dependent on the workload so it's not
    preferable to always enable. Therefore, make it optional in Kconfig
    and device param. Default is 'off'. This option will be beneficial
    for users who use the zram as blockdev and stores build output to it.
    
    Change-Id: If282bb8aa15c5749859a87cf36db7eb9edb3b1ed
    Reviewed-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Acked-by: Minchan Kim <minchan@kernel.org>
    Signed-off-by: Joonsoo Kim <iamjoonsoo.kim@lge.com>
    Link: https://lore.kernel.org/patchwork/patch/787164/
    Patch-mainline: linux-kernel@ Thu, 11 May 2017 22:30:52
    Signed-off-by: Charan Teja Reddy <charante@codeaurora.org>
    [swatsrid@codeaurora.org: Fix trivial merge conflicts]
    Signed-off-by: Swathi Sridhar <swatsrid@codeaurora.org>

--- drivers/block/zram/zram_drv.h
+++ drivers/block/zram/zram_drv.h
@@ -158,6 +158,7 @@ struct zram {
 	 * zram is claimed so open request will be failed
 	 */
 	bool claim; /* Protected by bdev->bd_mutex */
+	bool use_dedup;
 	struct file *backing_dev;
 #if defined(CONFIG_ZRAM_WRITEBACK) || defined(CONFIG_RTMM)
 	spinlock_t wb_limit_lock;
@@ -181,6 +182,15 @@ struct zram {
 
 /* mlog */
 unsigned long zram_mlog(void);
+
+static inline bool zram_dedup_enabled(struct zram *zram)
+{
+#ifdef CONFIG_ZRAM_DEDUP
+	return zram->use_dedup;
+#else
+	return false;
+#endif
+}
 
 void zram_entry_free(struct zram *zram, struct zram_entry *entry);
 #endif
