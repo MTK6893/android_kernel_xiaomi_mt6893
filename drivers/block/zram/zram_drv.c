986cbc8cdc037165ed2b7d3c4b8259efe11f71d3
jsdizkcksv <3531236823@qq.com>
2025-03-13 01:57:39 +0800

    Revert "zram: show deduped status in debugfs"
    This reverts commit 150b693c125c9fe32ffa5252aa636daa053db02d.

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1189,14 +1189,13 @@ static ssize_t read_block_state(struct file *file, char __user *buf,
 
 		ts = ktime_to_timespec64(zram->table[index].ac_time);
 		copied = snprintf(kbuf + written, count,
-			"%12zd %12lld.%06lu %c%c%c%c%c\n",
+			"%12zd %12lld.%06lu %c%c%c%c\n",
 			index, (s64)ts.tv_sec,
 			ts.tv_nsec / NSEC_PER_USEC,
 			zram_test_flag(zram, index, ZRAM_SAME) ? 's' : '.',
 			zram_test_flag(zram, index, ZRAM_WB) ? 'w' : '.',
 			zram_test_flag(zram, index, ZRAM_HUGE) ? 'h' : '.',
-			zram_test_flag(zram, index, ZRAM_IDLE) ? 'i' : '.',
- 			zram_test_flag(zram, index, ZRAM_DEDUPED) ? 'd' : '.');
+			zram_test_flag(zram, index, ZRAM_IDLE) ? 'i' : '.');
 
 		if (count < copied) {
 			zram_slot_unlock(zram, index);
@@ -1750,9 +1749,6 @@ static void zram_free_page(struct zram *zram, size_t index)
 		zram_clear_flag(zram, index, ZRAM_IDLE);
 		zram_clear_idle_count(zram, index);
 	}
-	
-	if (zram_test_flag(zram, index, ZRAM_DEDUPED))
- 		zram_clear_flag(zram, index, ZRAM_DEDUPED);
 
 	if (zram_test_flag(zram, index, ZRAM_HUGE)) {
 		zram_clear_flag(zram, index, ZRAM_HUGE);
@@ -1910,13 +1906,6 @@ static int __zram_bvec_write(struct zram *zram, struct bio_vec *bvec,
 		goto out;
 	}
 	kunmap_atomic(mem);
-	
-	entry = zram_dedup_find(zram, page, &checksum);
- 	if (entry) {
- 		flags = ZRAM_DEDUPED;
- 		comp_len = entry->len;
- 		goto out;
- 	}
 
 compress_again:
 	zstrm = zcomp_stream_get(zram->comp);
@@ -1997,17 +1986,11 @@ out:
 		atomic64_inc(&zram->stats.huge_pages);
 		atomic64_inc(&zram->stats.huge_pages_since);
 	}
-	
 
-	switch (flags) {
- 	case ZRAM_SAME:
+	if (flags) {
 		zram_set_flag(zram, index, flags);
 		zram_set_element(zram, index, element);
-			break;
- 	case ZRAM_DEDUPED:
- 		zram_set_flag(zram, index, flags);
- 		// Fallthrough
- 	default:
+	}  else {
 		zram_set_handle(zram, index, handle);
 		zram_set_obj_size(zram, index, comp_len);
 	}
