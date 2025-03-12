33556ead34f237ddf109dd30053150973b2b3ade
-Luo- <82682041+LuoxueQWQ@users.noreply.github.com>
2025-03-13 00:00:21 +0800

    sched/fair: Don't needlessly migrate a lone task to a higher capacity CPU
    When a CPU has only one running CFS task and there's a higher capacity CPU
    that's idle, that lone task may be migrated to the higher capacity CPU just
    because of RT and IRQ load.
    
    If the CPU running the lone CFS task has sufficient capacity for the task,
    then let it run that task. Migrating the task up to a higher capacity CPU
    causes that CPU to be kicked out of idle and degrades energy efficiency
    without an appreciable performance improvement. The load balancer will take
    care of migrating the task anyway if it becomes a misfit, so this heuristic
    isn't needed.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -10823,19 +10823,6 @@ static int need_active_balance(struct lb_env *env)
 			return 1;
 	}
 
-	/*
-	 * The dst_cpu is idle and the src_cpu CPU has only 1 CFS task.
-	 * It's worth migrating the task if the src_cpu's capacity is reduced
-	 * because of other sched_class or IRQs if more capacity stays
-	 * available on dst_cpu.
-	 */
-	if ((env->idle != CPU_NOT_IDLE) &&
-	    (env->src_rq->cfs.h_nr_running == 1)) {
-		if ((check_cpu_capacity(env->src_rq, sd)) &&
-		    (capacity_of(env->src_cpu)*sd->imbalance_pct < capacity_of(env->dst_cpu)*100))
-			return 1;
-	}
-
 	if ((capacity_of(env->src_cpu) < capacity_of(env->dst_cpu)) &&
 	    ((capacity_orig_of(env->src_cpu) < capacity_orig_of(env->dst_cpu))) &&
 				env->src_rq->cfs.h_nr_running == 1 &&
@@ -11930,15 +11917,6 @@ static inline bool nohz_kick_needed(struct rq *rq, bool only_update)
 			goto unlock;
 		}
 
-	}
-
-	sd = rcu_dereference(rq->sd);
-	if (sd) {
-		if ((rq->cfs.h_nr_running >= 1) &&
-				check_cpu_capacity(rq, sd)) {
-			kick = true;
-			goto unlock;
-		}
 	}
 
 	sd = rcu_dereference(per_cpu(sd_asym, cpu));
