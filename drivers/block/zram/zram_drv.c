ee17b836bd194d95e58cd04540b34370083d9fdb
yyq <3531236823@qq.com>
2025-03-13 17:20:13 +0800

    update: drivers/block/zram/zram_drv.c

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -755,8 +755,7 @@ static ssize_t writeback_store(struct device *dev,
 {
 	struct zram *zram = dev_to_zram(dev);
 	unsigned long nr_pages = zram->disksize >> PAGE_SHIFT;
-	unsigned long index, wb_max = ULONG_MAX;
-	unsigned int wb_idle_min = ZRAM_WB_IDLE_DEFAULT;
+	unsigned long index;
 	struct bio bio;
 	struct bio_vec bio_vec;
 	struct page *page;
